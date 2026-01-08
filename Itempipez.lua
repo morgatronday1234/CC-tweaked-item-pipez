739e5c72ef151ce4a2093096df8ef4b85cc31e86
morgatronday1234 <130381148+morgatronday1234@users.noreply.github.com>
2026-01-08 10:33:27 +0000

    Fix a dumb issue were I forgot to sync the dam files

--- Itempipez.lua
+++ Itempipez.lua
@@ -1,7 +1,3 @@
---This project is under the CC-BY-NC-4.0 license
---https://github.com/morgatronday1234
---Read the license!
-
 function move(input, output, nameFilter, inSlotFilter, outSlotFilter)
  assert(type(input) == "string", "Argument #1: Expected string(side), Got: "..tostring(input))
  assert(type(output) == "string", "Argument #2: Expected string(side), Got: "..tostring(input))
@@ -12,22 +8,26 @@ function move(input, output, nameFilter, inSlotFilter, outSlotFilter)
 
  assert(type(inputPer) == "table", "Argument #1: Expected peripheral, Got: '"..tostring(input).."'\n(Warning this may no-longer be a peripheral!)")
  
- if (inSlotFilter) and (type(inSlotFilter) == "number") then
-  fromSlot = inSlotFilter
+
+ if (inSlotFilter) and (inSlotFilter > inputPer.size()) then
+  return false, "slot greater then amount of slots"
  end
- if (outSlotFilter) and (type(outSlotFilter) == "number") then
-  toSlot = outSlotFilter
- end
- 
+
  for fromSlot, _ in pairs(inputPer.list()) do
+  if (inSlotFilter) and (type(inSlotFilter) == "number") then
+   fromSlot = inSlotFilter
+  end
+  if (outSlotFilter) and (type(outSlotFilter) == "number") then
+   toSlot = outSlotFilter
+  end 
   dataPerSlot = inputPer.getItemDetail(fromSlot)
 
-  
   if (nameFilter) and (dataPerSlot) and (dataPerSlot.displayName == nameFilter) then
-   inputPer.pushItems(output, fromSlot, 64, toSlot)
+   return true, inputPer.pushItems(output, fromSlot, 64, toSlot)
   elseif (nameFilter == nil) then
-   inputPer.pushItems(output, fromSlot, 64, toSlot)
+   return true, inputPer.pushItems(output, fromSlot, 64, toSlot)
   end
+  return false, "failed to move anything"
  end
  os.sleep(0.05)
 end
@@ -39,14 +39,10 @@ end
 @optional int, Input slot filter
 @optional int, Ouput slot filter
 --]]
---move("input", "output", ["nameFilter", inputSlotFilter, outputSlotFilter]) ([ ] stuff is not required, You can leave them as nil if you need on but not the rest)
+--move("input", "output", ["nameFilter", inputSlotFilter, outputSlotFilter]) (nameFilter is not required)
 function main() while(true) do
 
 end end
 --main() 
 --Val if you want to run this code without startup remove the comment above this one to call main().
 --If you want to run this with startup just comment out it once more.
-
---This project is under the CC-BY-NC-4.0 license
---https://github.com/morgatronday1234
---Read the license!
