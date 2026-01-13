60bc8be855d10f752745610d32da1ad93168a45f
morgatronday1234 <130381148+morgatronday1234@users.noreply.github.com>
2026-01-13 21:37:42 +0000

    Update Itempipez.lua

--- Itempipez.lua
+++ Itempipez.lua
@@ -1,3 +1,7 @@
+--This project is under the CC-BY-4.0 license
+--https://github.com/morgatronday1234
+--Read the license!
+
 DEBUG = false
 function move(input, output, nameFilter, inSlotFilter, outSlotFilter)
  assert(type(input) == "string", "Argument #1: Expected string(side), Got: "..tostring(input))
@@ -50,3 +54,7 @@ end end
 main() 
 --Val if you want to run this code without startup remove the comment above this one to call main().
 --If you want to run this with startup just comment out it once more.
+
+--This project is under the CC-BY-4.0 license
+--https://github.com/morgatronday1234
+--Read the license!
